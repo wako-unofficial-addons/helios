9846b83c29a352e0c2766b3b810e03a8854052c6
Davy <davidreedmichigan@gmail.com>
2019-08-06 10:26:38 +0200

    split hashList

--- projects/plugin/src/plugin/services/real-debrid/forms/torrents/real-debrid-torrents-instant-availability.form.ts
+++ projects/plugin/src/plugin/services/real-debrid/forms/torrents/real-debrid-torrents-instant-availability.form.ts
@@ -1,10 +1,49 @@
 import { RealDebridApiService } from '../../services/real-debrid-api.service';
 import { RealDebridTorrentsInstantAvailabilityDto } from '../../dtos/torrents/real-debrid-torrents-instant-availability.dto';
+import { forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export class RealDebridTorrentsInstantAvailabilityForm {
   static submit(hashList: string[]) {
-    return RealDebridApiService.get<RealDebridTorrentsInstantAvailabilityDto>(
-      `/torrents/instantAvailability/${hashList.join('/')}`
-    );
+
+    const allGroups = [];
+    let hashGroup = [];
+    hashList.forEach(h => {
+      if (hashGroup.length > 50) {
+        allGroups.push(hashGroup);
+        hashGroup = [];
+      }
+      hashGroup.push(h);
+    });
+
+    if (hashGroup.length > 0) {
+      allGroups.push(hashGroup);
+    }
+
+    const obss = [];
+
+    allGroups.forEach(hashes => {
+      const urlSearchParam = new URLSearchParams();
+      hashes.forEach(h => {
+        urlSearchParam.append('items[]', h);
+      });
+
+      obss.push(
+        RealDebridApiService.get<RealDebridTorrentsInstantAvailabilityDto>(
+          `/torrents/instantAvailability/${hashes.join('/')}`
+        )
+      );
+    });
+
+    let dto = {} as RealDebridTorrentsInstantAvailabilityDto;
+
+    return forkJoin(...obss)
+      .pipe(map((dtos: RealDebridTorrentsInstantAvailabilityDto[]) => {
+
+        dtos.forEach(d => {
+          dto = Object.assign(dto, d);
+        });
+        return dto;
+      }));
   }
 }
