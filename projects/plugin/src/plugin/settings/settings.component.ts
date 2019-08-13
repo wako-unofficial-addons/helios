ac18074ca96c323b8a7f1a9366818065eef5e894
Davy <davidreedmichigan@gmail.com>
2019-08-13 09:23:10 +0200

    remove unused code

--- projects/plugin/src/plugin/settings/settings.component.ts
+++ projects/plugin/src/plugin/settings/settings.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { logData } from '../services/tools';
 import { ModalController } from '@ionic/angular';
 import { ProvidersComponent } from './providers/providers.component';
 import { CloudAccountListComponent } from './cloud-account/cloud-account-list/cloud-account-list.component';
@@ -9,13 +8,10 @@ import { CloudAccountListComponent } from './cloud-account/cloud-account-list/cl
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent implements OnInit {
-  constructor(private translate: TranslateService, private modalCtrl: ModalController) {}
-
-  ngOnInit() {
-    logData('Current lang', this.translate.currentLang);
-    logData('Test instant translate', this.translate.instant('settings.title'));
+export class SettingsComponent {
+  constructor(private translate: TranslateService, private modalCtrl: ModalController) {
   }
+
 
   async openProviders() {
     const modal = await this.modalCtrl.create({
