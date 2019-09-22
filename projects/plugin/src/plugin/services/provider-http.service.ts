5b16f384ee45b0e14dff2eaf60c90b77e7c04155
Davy <davidreedmichigan@gmail.com>
2019-09-22 13:41:07 +0200

    v2.1.6 - handle cloudflare

--- projects/plugin/src/plugin/services/provider-http.service.ts
+++ projects/plugin/src/plugin/services/provider-http.service.ts
@@ -1,7 +1,9 @@
-import { throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { WakoBaseHttpService, WakoHttpError } from '@wako-app/mobile-sdk';
 import { HeliosCacheService } from './provider-cache.service';
 import { logData } from './tools';
+
+declare const cordova: any;
 
 export class ProviderHttpService extends WakoBaseHttpService {
   static byPassCors = true;
@@ -17,11 +19,53 @@ export class ProviderHttpService extends WakoBaseHttpService {
   }
 
   static handleError(err) {
-    console.log('ProviderHttp err', err);
+    if (err instanceof WakoHttpError && err.status === 503 && err.response.match('Ray ID')) {
+      return this.cfBypasser(err);
+    }
+
+    logData('ProviderHttp err', err);
     if (err instanceof WakoHttpError) {
       logData('METHOD: ' + err.request.method);
       logData('URL: ' + err.request.url);
     }
+
     return throwError(err);
+  }
+
+  private static cfBypasser(err: WakoHttpError) {
+    if (!cordova['InAppBrowser']) {
+      return throwError(err);
+    }
+
+    return new Observable(observer => {
+      let timer = null;
+
+      logData('CF - Bypassing', err.request.url);
+      const inAppRef = cordova['InAppBrowser'].open(err.request.url, '_blank', 'location=yes,hidden=yes');
+
+      inAppRef.addEventListener('loadstop', () => {
+        inAppRef.executeScript(
+          {
+            code: "document.getElementsByTagName('html')[0].innerHTML"
+          },
+          htmls => {
+            const html: string = htmls.pop();
+            if (typeof html === 'string' && html.match('Ray ID') === null) {
+              logData('CF - Bypassed', html);
+              // not cloudflare anymore
+              clearTimeout(timer);
+              inAppRef.close();
+              observer.next(html);
+              observer.complete();
+            }
+          }
+        );
+      });
+
+      timer = setTimeout(() => {
+        inAppRef.close();
+        observer.error(err);
+      }, 10000);
+    });
   }
 }
