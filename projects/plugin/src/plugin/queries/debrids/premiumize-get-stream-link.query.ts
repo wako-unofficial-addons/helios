71d16ad37c1da3cdfe2168a497ec875282f82f31
Davy <davidreedmichigan@gmail.com>
2019-12-24 17:39:41 +0100

    get streamLinks in only one place

--- projects/plugin/src/plugin/queries/debrids/premiumize-get-stream-link.query.ts
+++ projects/plugin/src/plugin/queries/debrids/premiumize-get-stream-link.query.ts
@@ -1,157 +1,36 @@
-import { PreimumizeTransferDirectdlContentDto } from '../../services/premiumize/dtos/transfer/premiumize-transfer-directdl.dto';
 import { SourceQuery } from '../../entities/source-query';
-import { cleanTitle, getSupportedMedia, incrementEpisodeCode, isEpisodeCodeMatchesFileName } from '../../services/tools';
-import { of, throwError } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { StreamLink, StreamLinkSource } from '../../entities/stream-link-source';
+import { getSupportedMedia } from '../../services/tools';
+import { map } from 'rxjs/operators';
+import { StreamLinkSource } from '../../entities/stream-link-source';
+import { Link } from '../../entities/link';
+import { StreamLinksFromLinksQuery } from './stream-links-from-links.query';
 
 export class PremiumizeGetStreamLinkQuery {
-  static sortByNameAsc(files: PreimumizeTransferDirectdlContentDto[]) {
-    files.sort((stream1, stream2) => {
-      if (stream1.path === stream2.path) {
-        return 0;
-      }
-
-      return stream1.path > stream2.path ? 1 : -1;
-    });
-  }
-
-  static sortBySizeDesc(files: PreimumizeTransferDirectdlContentDto[]) {
-    files.sort((stream1, stream2) => {
-      if (+stream1.size === +stream2.size) {
-        return 0;
-      }
-
-      return +stream1.size > +stream2.size ? -1 : 1;
-    });
-  }
-
   static getData(streamLinkSource: StreamLinkSource, sourceQuery: SourceQuery) {
     return streamLinkSource.premiumizeTransferDirectdlDto.pipe(
-      switchMap(dto => {
+      map(dto => {
         if (dto.status !== 'success') {
-          return throwError(dto.message);
+          throw new Error(dto.message);
         }
 
-        const files: PreimumizeTransferDirectdlContentDto[] = [];
+        const links: Link[] = [];
 
-        let hasIncompatibleFile = false;
         dto.content.forEach(_file => {
           const ext = '.' + _file.link.split('.').pop().toLowerCase();
           const commonVideoExtensions = getSupportedMedia('video').split('|');
 
           if (commonVideoExtensions.includes(ext)) {
-            files.push(_file);
-          } else {
-            hasIncompatibleFile = true;
-          }
-        });
-
-        if (files.length === 0 && hasIncompatibleFile) {
-          return throwError(`The file ${dto.content[0].link} is not a video format`);
-        }
-
-        this.sortByNameAsc(files);
-
-        if (sourceQuery.episode) {
-          const streamLinks: StreamLink[] = []; // Current episode + next
-          let currentEpisodeFound = false;
-
-          let episodeCode = sourceQuery.episode.episodeCode;
-          let error = false;
-
-          const title = sourceQuery.episode.title + ' ' + sourceQuery.episode.episodeTitle;
-
-          const hasSampleInTitle = title.match('sample') !== null;
-
-          files.forEach(d => {
-            if (error) {
-              return;
-            }
-            const filename = d.path;
-            if (isEpisodeCodeMatchesFileName(episodeCode, filename)) {
-
-              if (!hasSampleInTitle && filename.match('sample') !== null) {
-                return;
-              }
-
-
-              currentEpisodeFound = true;
-              try {
-                episodeCode = incrementEpisodeCode(episodeCode);
-              } catch (e) {
-                error = true;
-              }
-
-              const streamLink = new StreamLink(streamLinkSource.title, d.link, d.path, !!d.stream_link, d.stream_link);
-
-              streamLinks.push(streamLink);
-            }
-          });
-
-          if (currentEpisodeFound) {
-            return of(streamLinks);
-          }
-        }
-
-        if (sourceQuery.movie) {
-          let file: PreimumizeTransferDirectdlContentDto = null;
-
-          this.sortBySizeDesc(files);
-
-          const filterTitle = cleanTitle(sourceQuery.movie.title);
-
-          const year = sourceQuery.movie.year;
-          files.forEach(_file => {
-            const names = _file.path.split('/');
-            if (names.length > 1) {
-              names.shift();
-            }
-
-            const title = cleanTitle(names.join('/'));
-
-            if (!file && title.indexOf('sample') === -1 && title.indexOf(filterTitle) !== -1 && ((year && title.indexOf(year.toString()) !== -1) || !year)) {
-              file = _file;
-            }
-          });
-
-          if (!file) {
-            files.forEach(__file => {
-              const _title = cleanTitle(__file.path);
-
-              if (!file && _title.indexOf('sample') === -1 && _title.indexOf(filterTitle) !== -1) {
-                file = __file;
-              }
+            links.push({
+              filename: _file.path.split('/').pop(),
+              size: _file.size,
+              streamLink: _file.stream_link,
+              url: _file.link,
+              servicePlayerUrl: null
             });
           }
-
-          if (!file && files.length === 1) {
-            file = files[0];
-          }
-
-          if (file) {
-            const streamLink = new StreamLink(streamLinkSource.title, file.link, file.path, !!file.stream_link, file.stream_link);
-
-            return of([streamLink]);
-          }
-        }
-
-        const streamLinks: StreamLink[] = [];
-
-        files.forEach(_file => {
-          const names = _file.path.split('/');
-          if (names.length > 1) {
-            names.shift();
-          }
-
-          const title = names.join('/');
-
-          const streamLink = new StreamLink(title, _file.link, title, !!_file.stream_link, _file.stream_link);
-
-          streamLinks.push(streamLink);
         });
 
-        return of(streamLinks);
+        return StreamLinksFromLinksQuery.getData(streamLinkSource, sourceQuery, links);
       })
     );
   }
--- projects/plugin/src/plugin/queries/debrids/real-debrid-get-stream-link.query.ts
+++ projects/plugin/src/plugin/queries/debrids/real-debrid-get-stream-link.query.ts
@@ -1,108 +1,29 @@
 import { SourceQuery } from '../../entities/source-query';
-import { incrementEpisodeCode, isEpisodeCodeMatchesFileName } from '../../services/tools';
-import { RealDebridUnrestrictLinkDto } from '../../services/real-debrid/dtos/unrestrict/real-debrid-unrestrict-link.dto';
-import { StreamLink, StreamLinkSource } from '../../entities/stream-link-source';
+import { StreamLinkSource } from '../../entities/stream-link-source';
 import { map } from 'rxjs/operators';
+import { Link } from '../../entities/link';
+import { StreamLinksFromLinksQuery } from './stream-links-from-links.query';
 
 export class RealDebridGetStreamLinkQuery {
-  static sortByNameAsc(files: RealDebridUnrestrictLinkDto[]) {
-    files.sort((stream1, stream2) => {
-      if (stream1.filename === stream2.filename) {
-        return 0;
-      }
-
-      return stream1.filename > stream2.filename ? 1 : -1;
-    });
-  }
-
-  static sortBySizeDesc(files: RealDebridUnrestrictLinkDto[]) {
-    files.sort((stream1, stream2) => {
-      if (+stream1.filesize === +stream2.filesize) {
-        return 0;
-      }
-
-      return +stream1.filesize > +stream2.filesize ? -1 : 1;
-    });
-  }
 
   static getData(streamLinkSource: StreamLinkSource, sourceQuery: SourceQuery) {
     return streamLinkSource.realDebridLinks.pipe(
-      map(links => {
-        if (sourceQuery.episode) {
-          this.sortByNameAsc(links);
+      map(files => {
 
-          const streamLinks: StreamLink[] = []; // Current episode + next
-          let currentEpisodeFound = false;
-
-          let episodeCode = sourceQuery.episode.episodeCode;
-          let error = false;
-
-          links.forEach(link => {
-            if (error) {
-              return;
-            }
-
-            if (isEpisodeCodeMatchesFileName(episodeCode, link.filename)) {
-              currentEpisodeFound = true;
-              try {
-                episodeCode = incrementEpisodeCode(episodeCode);
-              } catch (e) {
-                error = true;
-              }
-
-              const streamLink = new StreamLink(
-                streamLinkSource.title,
-                link.download,
-                link.filename,
-                !!link.streamable,
-                null,
-                `https://real-debrid.com/streaming-${link.id}`
-              );
-
-              streamLinks.push(streamLink);
-            }
-          });
-
-          if (currentEpisodeFound) {
-            return streamLinks;
+        const links: Link[] = [];
+        files.forEach(file => {
+          if (file.mimeType.match('video') !== null) {
+            links.push({
+              filename: file.filename,
+              size: file.filesize,
+              streamLink: file.streamable ? file.link : null,
+              url: file.link,
+              servicePlayerUrl: `https://real-debrid.com/streaming-${file.id}`
+            });
           }
-        }
-
-        let streamLink: StreamLink = null;
-
-        if (sourceQuery.movie) {
-          this.sortBySizeDesc(links);
-
-          let foundLink = links.shift();
-
-          if (foundLink) {
-            streamLink = new StreamLink(
-              streamLinkSource.title,
-              foundLink.download,
-              foundLink.filename,
-              !!foundLink.streamable,
-              null,
-              `https://real-debrid.com/streaming-${foundLink.id}`
-            );
-            return [streamLink];
-          }
-        }
-
-        const streamLinks: StreamLink[] = [];
-
-        links.forEach(link => {
-          const streamLink = new StreamLink(
-            streamLinkSource.title,
-            link.download,
-            link.filename,
-            !!link.streamable,
-            null,
-            `https://real-debrid.com/streaming-${link.id}`
-          );
-          streamLinks.push(streamLink);
         });
 
-        return streamLinks;
+        return StreamLinksFromLinksQuery.getData(streamLinkSource, sourceQuery, links);
       })
     );
   }
