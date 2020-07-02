3373005e590d34733757e960e92f75f0697a45de
Davy <davidreedmichigan@gmail.com>
2020-07-02 14:20:09 +0200

    v4.0.2 - bug fixes

--- projects/plugin/src/plugin/services/real-debrid/commands/real-debrid-cache-url.command.ts
+++ projects/plugin/src/plugin/services/real-debrid/commands/real-debrid-cache-url.command.ts
@@ -1,18 +1,24 @@
 import { RealDebridTorrentsAddMagnetForm } from '../forms/torrents/real-debrid-torrents-add-magnet.form';
 import { retry, switchMap } from 'rxjs/operators';
 import { RealDebridTorrentsInfoForm } from '../forms/torrents/real-debrid-torrents-info.form';
-import { throwError } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { RealDebridTorrentsSelectFilesForm } from '../forms/torrents/real-debrid-torrents-select-files.form';
+import { RealDebridTorrentsDeleteForm } from '../forms/torrents/real-debrid-torrents-delete.form';
+import { RealDebridTorrentsAddMagnetDto } from '../dtos/torrents/real-debrid-torrents-add-magnet.dto';
 
 export class RealDebridCacheUrlCommand {
   static handle(url: string) {
+    let torrent: RealDebridTorrentsAddMagnetDto = null;
     return RealDebridTorrentsAddMagnetForm.submit(url).pipe(
-      switchMap(t => {
+      switchMap((t) => {
+        torrent = t;
         return RealDebridTorrentsInfoForm.submit(t.id);
       }),
-      switchMap(info => {
+      switchMap((info) => {
         if (info.files.length === 0) {
-          return throwError('Cannot add this source');
+          RealDebridTorrentsDeleteForm.submit(torrent.id).subscribe();
+
+          return throwError('Cannot add this source: ' + info.status);
         }
 
         return RealDebridTorrentsSelectFilesForm.submit(info.id, 'all').pipe(
