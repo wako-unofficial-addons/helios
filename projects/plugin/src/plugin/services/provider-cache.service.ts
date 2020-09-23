b9c42b67c8d92a24d742d094f3f2125841c6c059
Davy <davidreedmichigan@gmail.com>
2020-09-23 12:41:32 +0200

    Upgrade deps

--- projects/plugin/src/plugin/services/provider-cache.service.ts
+++ projects/plugin/src/plugin/services/provider-cache.service.ts
@@ -1,8 +1,12 @@
+import { PLATFORM_ID } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { WakoCacheService } from '@wako-app/mobile-sdk';
 
 export class HeliosCacheService extends WakoCacheService {
-  protected static storageEngine = new Storage({
-    name: 'wako_helios_cache'
-  });
+  protected static storageEngine = new Storage(
+    {
+      name: 'wako_helios_cache'
+    },
+    PLATFORM_ID
+  );
 }
