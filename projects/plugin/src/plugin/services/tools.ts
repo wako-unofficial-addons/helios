28a683f803d07b190ff9228ddb8ef6daba5f3266
Davy <davidreedmichigan@gmail.com>
2020-12-13 11:19:48 +0100

    add type

--- projects/plugin/src/plugin/services/tools.ts
+++ projects/plugin/src/plugin/services/tools.ts
@@ -26,7 +26,7 @@ export function countryCodeToEmoji(country: string) {
   return String.fromCodePoint(f + offset) + String.fromCodePoint(s + offset);
 }
 
-export function getSupportedMedia(media) {
+export function getSupportedMedia(media: 'video' | 'music' | 'picture') {
   if (media === 'video') {
     return '.m4v|.3g2|.3gp|.nsv|.tp|.ts|.ty|.strm|.pls|.rm|.rmvb|.mpd|.m3u|.m3u8|.ifo|.mov|.qt|.divx|.xvid|.bivx|.vob|.nrg|.img|.iso|.pva|.wmv|.asf|.asx|.ogm|.m2v|.avi|.bin|.dat|.mpg|.mpeg|.mp4|.mkv|.mk3d|.avc|.vp3|.svq3|.nuv|.viv|.dv|.fli|.flv|.rar|.001|.wpl|.vdr|.dvr-ms|.xsp|.mts|.m2t|.m2ts|.evo|.ogv|.sdp|.avs|.rec|.url|.pxml|.vc1|.h264|.rcv|.rss|.mpls|.webm|.bdmv|.wtv|.pvr|.disc';
   }
