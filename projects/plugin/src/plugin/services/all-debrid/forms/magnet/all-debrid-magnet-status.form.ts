55b11689123a3564cfea502b788b025017ccadd3
Davy <davidreedmichigan@gmail.com>
2020-12-13 19:52:14 +0100

    Handle AD file explorer feature

--- projects/plugin/src/plugin/services/all-debrid/forms/magnet/all-debrid-magnet-status.form.ts
+++ projects/plugin/src/plugin/services/all-debrid/forms/magnet/all-debrid-magnet-status.form.ts
@@ -2,11 +2,14 @@ import { AllDebridApiService } from '../../services/all-debrid-api.service';
 import { AllDebridMagnetStatusDto } from '../../dtos/magnet/all-debrid-magnet-status.dto';
 
 export class AllDebridMagnetStatusForm {
-  static submit(id) {
-
-    return AllDebridApiService.get<AllDebridMagnetStatusDto>('/magnet/status', {
-      id: id
-    }, null);
-
+  static submit(id?: any, status?: 'active' | 'ready' | 'expired' | 'error') {
+    return AllDebridApiService.get<AllDebridMagnetStatusDto>(
+      '/magnet/status',
+      {
+        id,
+        status
+      },
+      null
+    );
   }
 }
