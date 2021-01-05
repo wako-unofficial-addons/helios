b5f94272fcaf61d1fecac547d689489ecf933bd5
Davy <davidreedmichigan@gmail.com>
2021-01-05 05:26:47 +0100

    Upgrade deps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{ path: '', loadChildren: () => import('./tabs/tabs.module').then((m) => m.TabsPageModule) }];
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
