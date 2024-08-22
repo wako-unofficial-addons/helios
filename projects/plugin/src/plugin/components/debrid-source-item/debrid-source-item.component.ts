91f6aa65528a75ed5084e93c03d53357b4dea32b
Davy <davidreedmichigan@gmail.com>
2024-08-22 22:00:43 +0200

    chore: Update font size in debrid-source-item and torrent-source-item components

--- projects/plugin/src/plugin/components/debrid-source-item/debrid-source-item.component.ts
+++ projects/plugin/src/plugin/components/debrid-source-item/debrid-source-item.component.ts
@@ -1,43 +1,43 @@
 import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { IonBadge, IonCol, IonGrid, IonIcon, IonRippleEffect, IonRow } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { cubeOutline, ellipsisVerticalOutline, trophyOutline } from 'ionicons/icons';
 import { KodiOpenMedia } from '../../entities/kodi-open-media';
 import { SourceQuery } from '../../entities/source-query';
 import { StreamLinkSource } from '../../entities/stream-link-source';
 import { FileSizePipe } from '../../services/file-size.pipe';
 import { OpenSourceService } from '../../services/open-source.service';
-import { addIcons } from "ionicons";
-import { trophyOutline, cubeOutline, ellipsisVerticalOutline } from "ionicons/icons";
-import { IonRippleEffect, IonIcon, IonGrid, IonRow, IonCol, IonBadge } from "@ionic/angular/standalone";
 
 @Component({
-    selector: 'wk-debrid-source-item',
-    templateUrl: 'debrid-source-item.component.html',
-    styleUrls: ['debrid-source-item.component.scss'],
-    standalone: true,
-    imports: [NgIf, FileSizePipe, IonRippleEffect, IonIcon, IonGrid, IonRow, IonCol, IonBadge],
+  selector: 'wk-debrid-source-item',
+  templateUrl: './debrid-source-item.component.html',
+  styleUrls: ['./debrid-source-item.component.scss'],
+  standalone: true,
+  imports: [NgIf, FileSizePipe, IonRippleEffect, IonIcon, IonGrid, IonRow, IonCol, IonBadge],
 })
 export class DebridSourceItemComponent {
-    @Input()
-    source: StreamLinkSource;
+  @Input()
+  source: StreamLinkSource;
 
-    @Input()
-    sourceQuery: SourceQuery;
+  @Input()
+  sourceQuery: SourceQuery;
 
-    @Input()
-    kodiOpenMedia: KodiOpenMedia;
+  @Input()
+  kodiOpenMedia: KodiOpenMedia;
 
-    @Input()
-    isBestSource: boolean;
+  @Input()
+  isBestSource: boolean;
 
-    constructor(private openSourceService: OpenSourceService) {
-        addIcons({ trophyOutline, cubeOutline, ellipsisVerticalOutline });
-    }
+  constructor(private openSourceService: OpenSourceService) {
+    addIcons({ trophyOutline, cubeOutline, ellipsisVerticalOutline });
+  }
 
-    async download() {
-        await this.openSourceService.openStreamLinkSource(this.source, this.sourceQuery, this.kodiOpenMedia, 'default');
-    }
+  async download() {
+    await this.openSourceService.openStreamLinkSource(this.source, this.sourceQuery, this.kodiOpenMedia, 'default');
+  }
 
-    async more() {
-        await this.openSourceService.openStreamLinkSource(this.source, this.sourceQuery, this.kodiOpenMedia, 'more');
-    }
+  async more() {
+    await this.openSourceService.openStreamLinkSource(this.source, this.sourceQuery, this.kodiOpenMedia, 'more');
+  }
 }
